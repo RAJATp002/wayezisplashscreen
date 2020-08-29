b115f9651daf69c2be1f1f5b97841ae32db4a41c
Rajat payal <rajatpayal@outlook.in>
2020-08-30 01:31:05 +0530

    slapscreen by surfaceview

---
+++ app/src/main/java/com/example/wayezisplashscreen/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.wayezisplashscreen;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class MainActivity extends Activity {
+
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.nastnigh);
+
+        SurfaceView sv = (SurfaceView) findViewById(R.id.surfaceView2);
+        SurfaceHolder holder = sv.getHolder();
+        holder.addCallback(new Callback(){
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) { }
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mp.setDisplay(holder);
+                mp.start();
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) { }
+        });
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        if(null != mp) mp.release();
+        mp = null;
+    }
+}
